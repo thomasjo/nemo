7ecfc20f0c8d14d57fad3c30ccfa24220e808610
Thomas Johansen <thomasjo@gmail.com>
2019-03-18 12:08:30 +0100

    Refactor preprocessing logic

--- python/preprocess-data.py
+++ python/preprocess-data.py
@@ -71,33 +71,37 @@ for image_file in sorted(raw_data_dir.rglob("*.tiff")):
 
     output_file = processed_data_dir / "{}.png".format(image_file.stem)
 
-    # Segment/binarize using OpenCV...
+    # Binary mask for finding the "metal border".
     image = _imread(image_file)
     image_binary = binary(image, blur_size=75, threshold=70)
 
-    # Find and remove the metal "border".
-    labels = np.zeros_like(image_binary)
-    _, labels = cv.connectedComponents(image_binary, labels)
-    unique, counts = np.unique(labels[labels > 0], return_counts=True)
-    idx_max = np.argmax(counts)
-    label_max = unique[idx_max]
-    image_binary = binary(image, blur_size=31, threshold=130)
-    image_binary[labels == label_max] = 0
+    # Find the "metal border" component based on area.
+    n_labels, image_cc, stats, centroids = cv.connectedComponentsWithStats(image_binary)
+    # Assume that the metal border is the component with the largest area,
+    # after ignoring the "background" that is always labeled as 0.
+    border_label = np.argmax(stats[1:, cv.CC_STAT_AREA]) + 1
+
+    # Binary mask used for finding objects.
+    image_binary = binary(image, blur_size=31, threshold=120)
+
+    # Remove the "metal border" from the object mask.
+    image_binary[image_cc == border_label] = 0
 
     # Find all regions of interest.
-    labels = np.zeros_like(image_binary)
-    _, labels = cv.connectedComponents(image_binary, labels)
-    unique, counts = np.unique(labels[labels > 0], return_counts=True)
-    image_binary[np.isin(labels, unique[counts < 1024])] = 0
+    n_labels, image_cc, stats, centroids = cv.connectedComponentsWithStats(image_binary)
+
+    # Remove objects with fewer than specified number of pixels.
+    labels, pixel_counts = np.unique(image_cc[image_cc > 0], return_counts=True)
+    image_binary[np.isin(image_cc, labels[pixel_counts < 1024])] = 0
+
+    # Save object mask image.
     _imwrite(image_file, image_binary, suffix="binary")
 
-    # Create an image with ROIs overlayed. Useful for visual debugging.
+    # Create an image with mask overlays. Useful for visual debugging.
     image_seg = image.copy()
-    print(image_seg.shape)
     image_seg[image_binary == 255, 2] = 255
     image_seg[image_binary == 255, 1] = 0
     image_seg[image_binary == 255, 0] = 127
-
     alpha = 0.5
     image_overlay = cv.addWeighted(image_seg, alpha, image, 1 - alpha, 0)
     _imwrite(image_file, image_overlay, suffix="overlay")
