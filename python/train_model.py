fbf0ed6a75af2ff6e5726846402d0df46de9d307
Thomas Johansen <thomasjo@gmail.com>
2019-03-19 16:59:05 +0100

    Format code using black and isort

--- python/train_model.py
+++ python/train_model.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
-
-import matplotlib.pyplot as plt
-
+from tensorflow.keras.applications import VGG16
 
 # Used for auto-tuning dataset prefetching parameters.
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -56,39 +55,29 @@ path_ds = tf.data.Dataset.from_tensor_slices((train_paths, train_labels))
 image_label_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
 BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = train_count
 
-ds = image_label_ds.shuffle(buffer_size=train_count)
-ds = ds.repeat()
-ds = ds.batch(BATCH_SIZE)
-ds = ds.prefetch(buffer_size=AUTOTUNE)
-print(ds)
+train_batches = image_label_ds.shuffle(SHUFFLE_BUFFER_SIZE)
+train_batches = train_batches.batch(BATCH_SIZE)
+train_batches = train_batches.prefetch(AUTOTUNE)
+print(train_batches)
 
-base_model = tf.keras.applications.VGG16(weights="imagenet", include_top=False)
+base_model = VGG16(include_top=False, weights="imagenet", pooling="max")
 base_model.trainable = False
 print(base_model.summary())
 
-for image_batch, label_batch in ds.take(1):
-    pass
-print(image_batch.shape)
+for image_batch, label_batch in train_batches.take(1):
+    print(image_batch.shape)
 
 feature_batch = base_model(image_batch)
 print(feature_batch.shape)
 
-global_average_layer = keras.layers.GlobalAveragePooling2D()
-feature_batch_average = global_average_layer(feature_batch)
-print(feature_batch_average.shape)
-
 prediction_layer = keras.layers.Dense(1)
-prediction_batch = prediction_layer(feature_batch_average)
+prediction_batch = prediction_layer(feature_batch)
 print(prediction_batch.shape)
 
-model = keras.Sequential([
-    base_model,
-    global_average_layer,
-    prediction_layer,
-])
-
 base_learning_rate = 0.0001
+model = keras.Sequential([base_model, prediction_layer])
 model.compile(
     optimizer=keras.optimizers.RMSprop(lr=base_learning_rate),
     loss="binary_crossentropy",
