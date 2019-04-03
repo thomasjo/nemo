00b556c306481e134077252d51e58d65664296f6
Thomas Johansen <thomasjo@gmail.com>
2019-04-03 17:57:38 +0200

    Refactor and improve overall code quality

--- python/train_model.py
+++ python/train_model.py
@@ -6,9 +6,11 @@ import tensorflow as tf
 import tensorflow.keras as keras
 
 from tensorflow.keras.applications import VGG16
+from tensorflow.keras.optimizers import RMSprop
 
-# Used for auto-tuning dataset prefetching parameters.
+# Used for auto-tuning dataset prefetch size, etc.
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 32
 
 
 def preprocess_image(image):
@@ -23,6 +25,8 @@ def load_and_preprocess_image(path, label):
     image = tf.io.read_file(path)
     image = preprocess_image(image)
 
+    if label is None:
+        return image
     return image, label
 
 
@@ -37,102 +41,81 @@ def image_paths_and_labels(path, label_to_index):
     return image_paths, image_labels
 
 
-if __name__ == "__main__":
-    # TODO: Make this configurable via arguments using docopt.
-    data_dir = Path("/root/data")
+def batched_dataset(source_dir, label_to_index, shuffle=True):
+    paths, labels = image_paths_and_labels(source_dir, label_to_index)
+    count = len(paths)
 
+    dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
+    dataset = dataset.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+    if shuffle:
+        dataset = dataset.shuffle(count)
+
+    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.prefetch(AUTOTUNE)
+
+    return dataset, count
+
+
+def process_labels(source_dir):
+    label_names = sorted(path.name for path in source_dir.glob("*/") if path.is_dir())
+    label_to_index = dict((name, index) for index, name in enumerate(label_names))
+
+    return label_names, label_to_index
+
+
+if __name__ == "__main__":
+    file_dir = Path(__file__).parent.resolve()
+    root_dir = file_dir.parent
+
+    # TODO: Make these configurable?
+    data_dir = root_dir / "data"
     train_dir = data_dir / "train"
     test_dir = data_dir / "test"
 
-    label_names = sorted(path.name for path in train_dir.glob("*/") if path.is_dir())
-    print("label names:", label_names)
+    # Fetch label names, and a map from names to indices.
+    label_names, label_to_index = process_labels(train_dir)
 
-    label_to_index = dict((name, index) for index, name in enumerate(label_names))
-    print(label_to_index)
+    # Prepare batched training dataset.
+    train_batches, train_count = batched_dataset(train_dir, label_to_index)
 
-    train_paths, train_labels = image_paths_and_labels(train_dir, label_to_index)
-    train_count = len(train_paths)
-    print("num training images:", train_count)
-
-    test_paths, test_labels = image_paths_and_labels(test_dir, label_to_index)
-    test_count = len(test_paths)
-    print("num test images:", test_count)
-
-    raw_train = tf.data.Dataset.from_tensor_slices((train_paths, train_labels))
-    raw_test = tf.data.Dataset.from_tensor_slices((test_paths, test_labels))
-
-    train_ds = raw_train.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-    test_ds = raw_test.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-
-    BATCH_SIZE = 32
-    SHUFFLE_BUFFER_SIZE = train_count
-
-    train_batches = train_ds.shuffle(SHUFFLE_BUFFER_SIZE)
-    train_batches = train_batches.batch(BATCH_SIZE)
-    train_batches = train_batches.prefetch(AUTOTUNE)
-    print(train_batches)
-
-    test_batches = test_ds.batch(BATCH_SIZE)
-    test_batches = test_batches.prefetch(AUTOTUNE)
-    print(test_batches)
-
-    print()
-    print()
-
+    # Load a pre-trained base model to use for feature extraction.
     base_model = VGG16(include_top=False, weights="imagenet", pooling="max")
     base_model.trainable = False
-    base_model.summary()
 
-    for image_batch, label_batch in train_batches.take(1):
-        print(image_batch.shape)
-
-    feature_batch = base_model(image_batch)
-    print(feature_batch.shape)
-    print()
-
+    # Create model by stacking a prediction layer on top of the base model.
     prediction_layer = keras.layers.Dense(1)
-    prediction_batch = prediction_layer(feature_batch)
-    print(prediction_batch.shape)
-    print()
-
-    base_learning_rate = 0.0005
     model = keras.Sequential([base_model, prediction_layer])
-    model.compile(
-        optimizer=keras.optimizers.RMSprop(lr=base_learning_rate),
-        loss="binary_crossentropy",
-        metrics=["accuracy"],
-    )
+    model.compile(optimizer=RMSprop(lr=0.0005), loss="binary_crossentropy", metrics=["accuracy"])
     model.summary()
 
-    loss0, accuracy0 = model.evaluate(train_batches, steps=20)
+    print("\nEvaluating model before training...")
+    loss0, accuracy0 = model.evaluate(train_batches)
     print("initial loss: {:.2f}".format(loss0))
     print("initial accuracy: {:.2f}".format(accuracy0))
 
+    print("\nTraining model...")
+
+    # Initial training parameters.
     initial_epochs = 50
-    steps_per_epoch = round(train_count) // BATCH_SIZE
-    validation_split = 0.1
-    validation_steps = 20
+    steps_per_epoch = train_count // BATCH_SIZE
 
     history = model.fit(
-        train_batches.repeat(),
-        epochs=initial_epochs,
-        steps_per_epoch=steps_per_epoch,
-        # validation_split=validation_split,
-        # validation_steps=validation_steps,
+        train_batches.repeat(), epochs=initial_epochs, steps_per_epoch=steps_per_epoch
     )
 
-    loss, accuracy = model.evaluate(test_batches, steps=20)
+    # Prepare batched test dataset.
+    test_batches, _ = batched_dataset(test_dir, label_to_index, shuffle=False)
+
+    print("\nEvaluating model after training...")
+    loss, accuracy = model.evaluate(test_batches)
     print("final loss: {:.2f}".format(loss))
     print("final accuracy: {:.2f}".format(accuracy))
 
-    output_dir = Path("/root/output")
+    output_dir = root_dir / "output"
     output_dir.mkdir(parents=True, exist_ok=True)
 
     timestamp = datetime.utcnow()
     timestamp = timestamp.strftime("Ymd")
     model_file = output_dir / "nemo-{}-{:.2f}.h5".format(timestamp, accuracy)
-    model.save(str(model_file))
-
-    # Temporary...
-    model_file = output_dir / "nemo.h5".format(timestamp, accuracy)
     model.save(str(model_file))
