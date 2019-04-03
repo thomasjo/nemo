1dfb3e342fd3e6fefc5db78331f7fc9c20752342
Thomas Johansen <thomasjo@gmail.com>
2019-04-03 19:03:03 +0200

    Reuse functions from training

--- python/run_model.py
+++ python/run_model.py
@@ -6,67 +6,22 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
 
-# Used for auto-tuning dataset prefetching parameters.
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-
-def preprocess_image(image):
-    image = tf.io.decode_png(image, channels=3)
-    image = tf.image.resize_with_pad(image, 224, 224)
-    image /= 255.0
-
-    return image
-
-
-def load_and_preprocess_image(path, label=None):
-    image = tf.io.read_file(path)
-    image = preprocess_image(image)
-
-    if label is not None:
-        return image, label
-
-    return image
-
-
-def image_paths_and_labels(path, label_to_index):
-    image_paths = []
-    image_labels = []
-
-    for file in path.rglob("*.png"):
-        image_paths.append(str(file))
-        image_labels.append(label_to_index[file.parent.name])
-
-    return image_paths, image_labels
-
+from train_model import batched_dataset, image_paths, process_labels
 
 if __name__ == "__main__":
-    # TODO: Make this configurable via arguments using docopt.
-    data_dir = Path("/root/data")
+    file_dir = Path(__file__).parent.resolve()
+    root_dir = file_dir.parent
+
+    # TODO: Make these configurable?
+    data_dir = root_dir / "data"
     test_dir = data_dir / "test-new"
 
-    label_names = sorted(path.name for path in test_dir.glob("*/") if path.is_dir())
-    print("label names:", label_names)
+    label_names, label_to_index = process_labels(test_dir)
+    test_batches, _ = batched_dataset(test_dir, label_to_index, shuffle=False)
 
-    label_to_index = dict((name, index) for index, name in enumerate(label_names))
-    print(label_to_index)
-
-    test_paths, test_labels = image_paths_and_labels(test_dir, label_to_index)
-    test_count = len(test_paths)
-    print("num test images:", test_count)
-
-    raw_test = tf.data.Dataset.from_tensor_slices((test_paths, test_labels))
-    test_ds = raw_test.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-
-    BATCH_SIZE = 32
-
-    test_batches = test_ds.batch(BATCH_SIZE)
-    test_batches = test_batches.prefetch(AUTOTUNE)
-    print(test_batches)
-
-    print()
-    print()
-
-    model = keras.models.load_model("/root/output/nemo.h5")
+    output_dir = root_dir / "output"
+    model_file = output_dir / "nemo.h5"
+    model = keras.models.load_model(str(model_file))
     model.summary()
 
     loss, accuracy = model.evaluate(test_batches)
@@ -78,28 +33,21 @@ if __name__ == "__main__":
     print()
 
     mixed_dir = data_dir / "test-mixed"
-    mixed_paths = [str(file) for file in mixed_dir.rglob("*.png")]
-    mixed_count = len(mixed_paths)
-    print("num mixed images:", mixed_count)
-
-    raw_mixed = tf.data.Dataset.from_tensor_slices(mixed_paths)
-    mixed_ds = raw_mixed.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-
-    mixed_batches = mixed_ds.batch(BATCH_SIZE)
-    mixed_batches = mixed_batches.prefetch(AUTOTUNE)
+    mixed_paths = image_paths(mixed_dir)
+    mixed_batches, mixed_count = batched_dataset(mixed_dir, shuffle=False)
 
     predictions = model.predict(mixed_batches)
     predictions[predictions >= 0] = 1
     predictions[predictions < 0] = 0
     predictions = predictions.astype(int)
-    print(predictions)
 
-    output_dir = Path("/root/output/predictions")
-    benthos_dir = output_dir / "benthos"
-    plankton_dir = output_dir / "plankton"
+    output_dir = root_dir / "output" / "predictions"
 
-    benthos_dir.mkdir(parents=True, exist_ok=True)
-    plankton_dir.mkdir(parents=True, exist_ok=True)
+    label_dirs = {}
+    for label_name in label_names:
+        label_dir = output_dir / label_name
+        label_dir.mkdir(parents=True, exist_ok=True)
+        label_dirs[label_name] = label_dir
 
     for i in range(mixed_count):
         image_path = mixed_paths[i]
@@ -107,9 +55,6 @@ if __name__ == "__main__":
 
         label = predictions[i, 0]
         label_name = label_names[label]
-        if label_name == "benthos":
-            target_file = benthos_dir / image_file.name
-        else:
-            target_file = plankton_dir / image_file.name
 
+        target_file = label_dirs[label_name] / image_file.name
         target_file.write_bytes(image_file.read_bytes())
