0a5dce7f15c8c1d465a7d884bc2b6fd3c50c0432
Thomas Johansen <thomasjo@gmail.com>
2019-09-17 18:00:16 +0200

    Add cmd arguments to model scripts

--- python/run_model.py
+++ python/run_model.py
@@ -1,9 +1,18 @@
+"""
+Usage:
+  run_model.py [options] <source> <output> <model>
+
+Options:
+  -h, --help  Show this screen.
+"""
+
 import shutil
 from pathlib import Path
 
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
+from docopt import docopt
 
 from datasets import labels_for_dir
 from images import load_and_preprocess_image
@@ -14,41 +23,31 @@ BATCH_SIZE = 32
 IMAGE_SIZE = 224
 
 
-if __name__ == "__main__":
-    file_dir = Path(__file__).parent.resolve()
-    root_dir = file_dir.parent
+def main(source_dir, output_dir, model_file):
+    # Prepare dataset.
+    files = sorted([str(file) for file in source_dir.rglob("*.png")])
+    dataset = tf.data.Dataset.from_tensor_slices(files)
+    dataset = dataset.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.prefetch(AUTOTUNE)
 
-    # TODO: Make these configurable?
-    data_dir = root_dir / "data"
-    mixed_dir = data_dir / "test-mixed"
-    output_dir = root_dir / "output"
+    # Read labels associated with the trained model.
+    labels = labels_for_dir(source_dir.parent / "train")
 
     # Load trained model.
-    # TODO: Make model path configurable.
-    model_file = output_dir / "nemo.h5"
     model = keras.models.load_model(str(model_file), compile=False)
     # model.summary()
 
-    # Prepare dataset.
-    mixed_files = sorted([str(file) for file in mixed_dir.rglob("*.png")])
-    mixed_dataset = tf.data.Dataset.from_tensor_slices(mixed_files)
-    mixed_dataset = mixed_dataset.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-    mixed_dataset = mixed_dataset.batch(BATCH_SIZE)
-    mixed_dataset = mixed_dataset.prefetch(AUTOTUNE)
-
     # Extract softmax class predictions.
-    predictions = model.predict(mixed_dataset)
+    predictions = model.predict(dataset)
 
     # Convert softmax predictions to "hard" predictions.
     predictions = np.argmax(predictions, axis=1)
 
     # Prepare directory for predictions.
-    result_dir = root_dir / "output" / "predictions"
+    result_dir = output_dir / "predictions"
     shutil.rmtree(result_dir, ignore_errors=True)
     result_dir.mkdir(parents=True)
-
-    labels = labels_for_dir(data_dir / "train")
-    assert sorted(labels.values()) == list(range(len(labels)))  # Sanity check
 
     # Prepare sub-directories for all dataset labels.
     label_dirs = {}
@@ -58,8 +57,17 @@ if __name__ == "__main__":
         label_dirs[i] = label_dir
 
     # Write copies of classified images in label-based directory scheme.
-    for i, image_path in enumerate(mixed_files):
+    for i, image_path in enumerate(files):
         image_label = predictions[i]
-        image_bytes = Path(mixed_files[i]).read_bytes()
+        image_bytes = Path(files[i]).read_bytes()
         target_file = label_dirs[image_label] / Path(image_path).name
         target_file.write_bytes(image_bytes)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    source_dir = Path(args["<source>"])
+    output_dir = Path(args["<output>"])
+    model_file = Path(args["<model>"])
+
+    main(source_dir, output_dir, model_file)
